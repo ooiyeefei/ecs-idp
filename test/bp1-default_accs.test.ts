e1f6049b43199c6e36d1384afc1944ca6b74205b
Ooi Yee Fei <yeefeiooi@gmail.com>
2024-05-19 12:15:44 +0800

    Initial commit

---
+++ test/bp1-default_accs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Bp1DefaultAccs from '../lib/bp1-default_accs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bp1-default_accs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Bp1DefaultAccs.Bp1DefaultAccsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
